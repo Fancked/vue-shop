0a3d3956e35fe4569554f50015c3a34d5d08d1c3
Fancked <Fancked@users.noreply.github.com>
2020-08-24 22:40:32 +0800

    ''

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -1,37 +1,39 @@
 <!--  -->
 <template>
-  <div class="">
-    <van-sticky :offset-top="0">
-      <van-nav-bar title="用户详情" />
-    </van-sticky>
+<div class="">
+  <van-sticky :offset-top="0">
+    <van-nav-bar title="用户详情" />
+  </van-sticky>
 
-    <van-cell-group>
-      <van-cell @click="loginClick">
-        <van-row>
-          <van-col :span="8">
-            <van-image :src="defaultPic" class="user-pic-img" fit="cover" round width="80%" />
-          </van-col>
-          <van-col>
-            <div class="login">
-              登录
-            </div>
-          </van-col>
-        </van-row>
-      </van-cell>
+  <van-cell-group>
+    <van-cell @click="loginClick">
+      <van-row>
+        <van-col :span="8">
+          <van-image :src="defaultPic" class="user-pic-img" fit="cover" round width="80%" />
+        </van-col>
+        <van-col>
+          <div class="login">
+            登录
+          </div>
+        </van-col>
+      </van-row>
+    </van-cell>
 
-      <van-cell title="切换城市" clickable />
-      <van-cell title="我的收藏" clickable />
-      <van-cell title="我的足迹" clickable />
-      <van-cell title="购买记录" clickable />
-    </van-cell-group>
-  </div>
+    <van-cell title="切换城市" clickable />
+    <van-cell title="我的收藏" clickable />
+    <van-cell title="我的足迹" clickable />
+    <van-cell title="购买记录" clickable />
+  </van-cell-group>
+</div>
 </template>
 
 <script>
 export default {
   components: {},
   data() {
-    return { defaultPic: '@/assets/img/profile/user.png' };
+    return {
+      defaultPic: 'https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1598251862&di=ff519fb002e9c57e709e77b61d657f84&src=http://a1.att.hudong.com/05/00/01300000194285122188000535877.jpg',
+    };
   },
   computed: {},
   watch: {},
@@ -51,6 +53,7 @@ export default {
   activated() {},
 };
 </script>
+
 <style lang="scss" scoped>
 .login {
   font-size: 1.4rem;
